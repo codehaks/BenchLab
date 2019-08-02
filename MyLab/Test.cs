bcea2e9d003ef57a79b0200fdf9df5da4cc00a60
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-03 01:39:51 +0430

    value

--- MyLab/Test.cs
+++ MyLab/Test.cs
@@ -1,22 +1,60 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyLab
 {
     public class Test
     {
+        public string _path = @"D:\Projects\Data\pin-payments.csv";
+        public string[] Payments { get; set; }
+        public string[] Users { get; set; }
+        public string selectedUser = "de638a1c-4074-4f8a-9a1f-49512726c628";
+
+        public Test()
+        {
+            Payments = System.IO.File.ReadAllLines(_path).Take(10_000).ToArray();
+            Users = new string[Payments.Length];
+
+            for (int i = 0; i < Payments.Length; i++)
+            {
+                var record = Payments[i].Split(',');
+                Users[i] = record[2];
+            }
+        }
+
         [Benchmark]
         public void Case1()
         {
+            var linkedList = new LinkedList<int>();
 
+            for (int i = 0; i < Payments.Length; i++)
+            {
+                var record = Payments[i].Split(',');
+                var value = int.Parse(record[4]) - int.Parse(record[3]);
+                
+                linkedList.AddLast(value);
+            }
+
+            Console.WriteLine($"Count : {linkedList.Sum()}");
         }
 
         [Benchmark]
         public void Case2()
         {
+            var list = new List<int>();
 
+            for (int i = 0; i < Payments.Length; i++)
+            {
+                var record = Payments[i].Split(',');
+
+                var value = int.Parse(record[4]) - int.Parse(record[3]);
+                list.Add(value);
+            }
+
+            Console.WriteLine($"Count : {list.Sum()}");
         }
     }
 }
