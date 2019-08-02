f9fd4ead11a82ee34e4dc8e6defc017c9ab14779
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-03 01:14:55 +0430

    Fix

--- MyLab/Test.cs
+++ MyLab/Test.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MyLab
@@ -14,7 +15,7 @@ namespace MyLab
 
         public Test()
         {
-            Payments = System.IO.File.ReadAllLines(_path);
+            Payments = System.IO.File.ReadAllLines(_path).Take(10_000).ToArray();
             Users = new string[Payments.Length];
 
             for (int i = 0; i < Payments.Length; i++)
@@ -44,16 +45,16 @@ namespace MyLab
         [Benchmark]
         public void Case2()
         {
-            var hashset = new HashSet<string>();
-            for (int i = 0; i < 10_000; i++)
-            {
-                var record = Payments[i].Split(',');
-                if (hashset.Contains(record[2]) == false)
-                {
-                    hashset.Add(record[2]);
-                }
-            }
-
+            var hashset = new HashSet<string>(Users);
+            //for (int i = 0; i < 10_000; i++)
+            //{
+            //    var record = Payments[i].Split(',');
+            //    if (hashset.Contains(record[2]) == false)
+            //    {
+            //        hashset.Add(record[2]);
+            //    }
+            //}
+            
             Console.WriteLine($"count: {hashset.Count}");
         }
     }
