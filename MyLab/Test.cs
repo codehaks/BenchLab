6c181ebe642dae527e15ad9c9cede8c13c35894c
Hakim Ghods <ghods.hakim@hotmail.com>
2019-08-04 02:45:43 +0430

    Fix

--- MyLab/Test.cs
+++ MyLab/Test.cs
@@ -10,13 +10,31 @@ namespace MyLab
         [Benchmark]
         public void Case1()
         {
-
-        }
+            var list = new List<int>();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                list.Add(i);
+            }
+        }        
 
         [Benchmark]
         public void Case2()
         {
+            var list = new LinkedList<int>();
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                list.AddLast(i);
+            }
+        }
 
+        [Benchmark]
+        public void Case3()
+        {
+            var list = new int[1_000_000];
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                list[i] = i;
+            }
         }
     }
 }
